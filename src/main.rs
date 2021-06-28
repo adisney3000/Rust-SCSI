ef687430713d8c271576c2f8d0f29ce0f49bbb92
Adam Disney <adisney3000@gmail.com>
2021-06-28 14:50:25 -0400

    write 6 finished

--- src/main.rs
+++ src/main.rs
@@ -81,69 +81,73 @@ fn print_usage(error: &str, program: &str, opts: Options) {
 }
 
 fn fill_commands(commands: &mut HashMap <&str, ShellCommand>) {
-  let mut opts = Options::new();
-  opts.optflag("?", "help", "");
-  opts.optflag("", "show_input", "display the inputs given to device");
-  opts.optflag("i", "immed", "immediate flag");
-  commands.insert("rewind", ShellCommand { func:rewind, options: opts });
+  let mut options = Options::new();
+  options.optflag("?", "help", "");
+  options.optflag("i", "immed", "immediate flag");
+  commands.insert("rewind", ShellCommand { func:rewind, options });
 
-  opts = Options::new();
-  opts.optflag("?", "help", "");
-  opts.optflag("", "show_input", "display the inputs given to device");
-  opts.optflag("s", "sili", "suppress incorrect-length indicator flag");
-  opts.optflag("f", "fixed", "read fixed size blocks flag");
-  opts.reqopt("l", "transfer_length", "number of bytes/blocks to read", "<u32>");
-  opts.optopt("", "output_file", "file to write output; stdout otherwise", "");
-  commands.insert("read_6", ShellCommand { func:read_6, options: opts });
+  options = Options::new();
+  options.optflag("?", "help", "");
+  options.optflag("s", "sili", "suppress incorrect-length indicator flag");
+  options.optflag("f", "fixed", "read fixed size blocks flag");
+  options.reqopt("l", "transfer_length", "number of bytes/blocks to read", "<u32>");
+  options.optopt("", "output_file", "file to write output; stdout otherwise", "<str>");
+  commands.insert("read_6", ShellCommand { func:read_6, options });
 
-  opts = Options::new();
-  opts.optflag("?", "help", "");
-  opts.optflag("", "show_input", "display the inputs given to device");
-  opts.optopt("a", "allow_overwrite", "allow overwrite type", "<u8>");
-  opts.optopt("p", "partition", "partition id", "<u8>");
-  opts.optopt("l", "logical_object_identifier", "", "<u64>");
+  options = Options::new();
+  options.optflag("?", "help", "");
+  options.optopt("a", "allow_overwrite", "allow overwrite type", "<u8>");
+  options.optopt("p", "partition", "partition id", "<u8>");
+  options.optopt("l", "logical_object_identifier", "", "<u64>");
   commands.insert("allow_overwrite",
-      ShellCommand { func:allow_overwrite, options: opts });
+      ShellCommand { func:allow_overwrite, options });
 
-  opts = Options::new();
-  opts.optflag("?", "help", "");
-  opts.optflag("", "show_input", "display the inputs given to device");
-  opts.optflag("", "fcs", "first command in sequence flag");
-  opts.optflag("", "lcs", "last command in sequence flag");
-  opts.optflag("i", "immed", "immediate flag");
-  opts.optflag("", "long", "long flag");
-  opts.optopt("m", "method", "erase method", "<u8>");
-  opts.optflag("", "smd", "security metadata flag");
-  opts.optflag("", "vcm", "vendor-specific control metadata flag");
-  opts.optopt("p", "partition", "partition id", "<u8>");
-  opts.optopt("l", "logical_object_identifier", "", "<u64>");
+  options = Options::new();
+  options.optflag("?", "help", "");
+  options.optflag("", "fcs", "first command in sequence flag");
+  options.optflag("", "lcs", "last command in sequence flag");
+  options.optflag("i", "immed", "immediate flag");
+  options.optflag("", "long", "long flag");
+  options.optopt("m", "method", "erase method", "<u8>");
+  options.optflag("", "smd", "security metadata flag");
+  options.optflag("", "vcm", "vendor-specific control metadata flag");
+  options.optopt("p", "partition", "partition id", "<u8>");
+  options.optopt("l", "logical_object_identifier", "", "<u64>");
   commands.insert("erase_16",
-      ShellCommand { func:erase_16, options: opts });
+      ShellCommand { func:erase_16, options });
 
-  opts = Options::new();
-  opts.optflag("?", "help", "");
-  opts.optflag("", "show_input", "display the inputs given to device");
-  opts.optflag("i", "immed", "immediate flag");
-  opts.optflag("", "long", "long flag");
-  opts.optopt("m", "method", "erase method", "<u8>");
-  opts.optflag("", "smd", "security metadata flag");
-  opts.optflag("", "vcm", "vendor-specific control metadata flag");
+  options = Options::new();
+  options.optflag("?", "help", "");
+  options.optflag("i", "immed", "immediate flag");
+  options.optflag("", "long", "long flag");
+  options.optopt("m", "method", "erase method", "<u8>");
+  options.optflag("", "smd", "security metadata flag");
+  options.optflag("", "vcm", "vendor-specific control metadata flag");
   commands.insert("erase_6",
-      ShellCommand { func:erase_6, options: opts });
+      ShellCommand { func:erase_6, options });
 
-  opts = Options::new();
-  opts.optflag("?", "help", "");
-  opts.optflag("", "show_input", "display the inputs given to device");
-  opts.optflag("v", "verify", "verify flag");
-  opts.optflag("i", "immed", "immediate flag");
-  opts.optopt("f", "format", "format field", "<u8>");
-  opts.optopt("l", "transfer_length", "transfer length of input buffer", "<u16>");
-  opts.optopt("", "input_file", "file containing binary input buffer", "");
+  options = Options::new();
+  options.optflag("?", "help", "");
+  options.optflag("", "show_input", "display the inputs given to device");
+  options.optflag("v", "verify", "verify flag");
+  options.optflag("i", "immed", "immediate flag");
+  options.optopt("f", "format", "format field", "<u8>");
+  options.optopt("l", "transfer_length", "transfer length of input buffer", "<u16>");
+  options.optopt("", "input_file", "file containing binary input", "<str>");
   commands.insert("format_medium",
-      ShellCommand { func:format_medium, options: opts });
+      ShellCommand { func:format_medium, options });
 
-  //opts = Options::new();
-  //opts.optflag("?", "help", "");
+  options = Options::new();
+  options.optflag("?", "help", "");
+  options.optflag("f", "fixed", "fixed flag");
+  options.optflag("", "show_input", "display the inputs given to device");
+  options.reqopt("l", "transfer_length", "number of bytes/blocks to write", "<u32>");
+  options.reqopt("", "input_file", "file containing binary input", "<str>");
+  commands.insert("write_6",
+      ShellCommand { func:write_6, options });
+
+  //options = Options::new();
+  //options.optflag("?", "help", "");
 }
 
 
@@ -199,9 +203,7 @@ fn rewind(device: &mut scsi::Device, opts: &Options, args: &[&str]) {
     immed: matches.opt_present("immed"),
   };
 
-  if matches.opt_present("show_input") {
-    println!("Issuing: {:#?}", cmd);
-  }
+  println!("Issuing: {:#?}", cmd);
   let result = device.issue_cmd(&cmd);
   print_status_or_error_and_return!(result, cmd);
 }
@@ -218,9 +220,7 @@ fn read_6(device: &mut scsi::Device, opts: &Options, args: &[&str]) {
   let mut buf: Vec <u8> = Vec::new();
   buf.resize(cmd.transfer_length as usize, 0);
   
-  if matches.opt_present("show_input") {
-    println!("Issuing: {:#?}", cmd);
-  }
+  println!("Issuing: {:#?}", cmd);
   let result = device.issue_cmd_with_output(&cmd, buf.as_mut_slice());
   print_status_or_error_and_return!(result, cmd);
 
@@ -258,9 +258,7 @@ fn allow_overwrite(device: &mut scsi::Device, opts: &Options, args: &[&str]) {
     logical_object_identifier: get_opt_or_return!("logical_object_identifier", matches),
   };
 
-  if matches.opt_present("show_input") {
-    println!("Issuing: {:#?}", cmd);
-  }
+  println!("Issuing: {:#?}", cmd);
   let result = device.issue_cmd(&cmd);
   print_status_or_error_and_return!(result, cmd);
 }
@@ -280,9 +278,7 @@ fn erase_16(device: &mut scsi::Device, opts: &Options, args: &[&str]) {
     logical_object_identifier: get_opt_or_return!("logical_object_identifier", matches),
   };
 
-  if matches.opt_present("show_input") {
-    println!("Issuing: {:#?}", cmd);
-  }
+  println!("Issuing: {:#?}", cmd);
   let result = device.issue_cmd(&cmd);
   print_status_or_error_and_return!(result, cmd);
 }
@@ -298,9 +294,7 @@ fn erase_6(device: &mut scsi::Device, opts: &Options, args: &[&str]) {
     vcm: matches.opt_present("vcm"),
   };
 
-  if matches.opt_present("show_input") {
-    println!("Issuing: {:#?}", cmd);
-  }
+  println!("Issuing: {:#?}", cmd);
   let result = device.issue_cmd(&cmd);
   print_status_or_error_and_return!(result, cmd);
 }
@@ -349,13 +343,12 @@ fn format_medium(device: &mut scsi::Device, opts: &Options, args: &[&str]) {
 
   println!("Issuing: {:#?}", cmd);
   if matches.opt_present("show_input") {
-    println!("Input buf: {:x?}", buf);
+    println!("Input buffer: {:x?}", buf);
   }
   let result = device.issue_cmd_with_input(&cmd, &buf);
   print_status_or_error_and_return!(result, cmd);
 }
 
-/*
 fn write_6(device: &mut scsi::Device, opts: &Options, args: &[&str]) {
   let matches = command_parse_args_or_return!("write_6", opts, args);
 
@@ -364,6 +357,7 @@ fn write_6(device: &mut scsi::Device, opts: &Options, args: &[&str]) {
     transfer_length: get_opt_or_return!("transfer_length", matches),
   };
 
+  //Allocate the buffer and fill in
   let buf_len = cmd.transfer_length.try_into();
   if buf_len.is_err() {
     eprintln!("failed to convert transfer_length to usize!");
@@ -377,7 +371,26 @@ fn write_6(device: &mut scsi::Device, opts: &Options, args: &[&str]) {
     eprintln!("failed to open {}: {}", input_file_name, e);
     return;
   }
-
   
+  match file.unwrap().read(&mut buf) {
+    Ok(read_bytes) => {
+      if read_bytes != buf.len() {
+        eprintln!("failed to read {} bytes from {}",
+            cmd.transfer_length, input_file_name);
+        return;
+      }
+    },
+    Err(e) => {
+      eprintln!("error occured while reading input file: {}", e);
+      return;
+    },
+  }
+
+  //Now issue the command
+  println!("Issuing: {:#?}", cmd);
+  if matches.opt_present("show_input") {
+    println!("Input buffer: {:x?}", buf);
+  }
+  let result = device.issue_cmd_with_input(&cmd, &buf);
+  print_status_or_error_and_return!(result, cmd);
 }
-*/
